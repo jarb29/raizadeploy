cf351d30656dd1426791448a232c77d7e7865dc5
JOSE RUBIO <jarb29@JOSE.local>
2024-05-10 19:02:47 -0400

    ading

--- src/app/dashboard/product/[id]/page.js
+++ src/app/dashboard/product/[id]/page.js
@@ -1,31 +1,31 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-import axios, { endpoints } from 'src/utils/axios';
+// import axios, { endpoints } from 'src/utils/axios';
 
-import { ProductDetailsView } from 'src/sections/product/view';
+// import { ProductDetailsView } from 'src/sections/product/view';
 
-// ----------------------------------------------------------------------
+// // ----------------------------------------------------------------------
 
-export const metadata = {
-  title: 'Dashboard: Product Details',
-};
+// export const metadata = {
+//   title: 'Dashboard: Product Details',
+// };
 
-export default function ProductDetailsPage({ params }) {
-  const { id } = params;
+// export default function ProductDetailsPage({ params }) {
+//   const { id } = params;
 
-  return <ProductDetailsView id={id} />;
-}
+//   return <ProductDetailsView id={id} />;
+// }
 
-export async function generateStaticParams() {
-  const res = await axios.get(endpoints.product.list);
+// export async function generateStaticParams() {
+//   const res = await axios.get(endpoints.product.list);
 
-  return res.data.products.map((product) => ({
-    id: product.id,
-  }));
-}
+//   return res.data.products.map((product) => ({
+//     id: product.id,
+//   }));
+// }
 
-ProductDetailsPage.propTypes = {
-  params: PropTypes.shape({
-    id: PropTypes.string,
-  }),
-};
+// ProductDetailsPage.propTypes = {
+//   params: PropTypes.shape({
+//     id: PropTypes.string,
+//   }),
+// };
